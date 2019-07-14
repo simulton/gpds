9a1ee83c89ce55d4f9f1011244efb9658a4a0cbc
Joel Bodenmann <joel.bodenmann@simulton.com>
2019-07-14 18:25:57 +0200

    Add string parsing test

---
+++ test/tests/test_04.cpp
@@ -0,0 +1,42 @@
+#include <vector>
+#include "catch2/catch.hpp"
+#include "../test.h"
+#include "gpds/serialize.h"
+
+static const std::string FILE_CONTENT =
+    "<?xml version=\"1.0\" encoding=\"utf-8\"?>"
+    "<data>"
+    "  <string>Hello World!</string>"
+    "</data>";
+
+class TestData : public Gpds::Serialize
+{
+public:
+    std::vector<std::string> data;
+
+    virtual Gpds::Container toContainer() const override
+    {
+        return { };
+    }
+
+    virtual void fromContainer( const Gpds::Container& object ) override
+    {
+        data = object.getValues<std::string>( "string" );
+    }
+};
+
+TEST_CASE( "Read Datatype: String" )
+{
+    // The "known good" data
+    const std::vector<std::string> knownGood = {
+        "Hello World!"
+    };
+
+    // Parse test file
+    TestData data;
+    REQUIRE( deserialize( FILE_CONTENT, data ) );
+
+    // Ensure that data is the same
+    REQUIRE( data.data == knownGood );
+}
+

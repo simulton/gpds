96e1c2c5e430eba32329e142b800e4ea158579a5
Joel Bodenmann <joel.bodenmann@simulton.com>
2019-07-29 16:36:04 +0200

    Remove trailing semicolon

--- lib/include/gpds/container.h
+++ lib/include/gpds/container.h
@@ -63,7 +63,7 @@ namespace Gpds
             attributes.add( std::forward< gString >( key ), value );
 
             return *this;
-        };
+        }
 
         template<typename T>
         std::optional<T> getAttribute(gString&& key) const

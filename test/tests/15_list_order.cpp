6676627f5c6ecc1615567ea6e96d98d65b4c60b9
Tibo Clausen <tibo.clausen@simulton.com>
2019-08-26 15:11:08 +0100

    Add test to guarantee that the order of the values stays consistent.
    Summary:
    1. Creates a Serializable object that contains a vector of integers.
    2. Fill vector with values
    3. Serialize it
    4. Create new object
    5. Deserialize into the new object
    6. Compare old and new object
    
    Ref T1462
    
    Test Plan:
    - Test passes
    - Altering one of the vectors before the comparison makes the test fail as expected.
    
    Reviewers: Joel
    
    Reviewed By: Joel
    
    Maniphest Tasks: T1462
    
    Differential Revision: https://phabricator.simulton.com/D102

---
+++ test/tests/15_list_order.cpp
@@ -0,0 +1,48 @@
+#include <sstream>
+#include "catch2/catch.hpp"
+#include "serialize.h"
+#include "../test.h"
+
+class ListOrder : public Gpds::Serialize
+{
+public:
+    std::vector<int> data;
+
+    virtual Gpds::Container toContainer() const override
+    {
+        Gpds::Container root;
+        for (int i: data){
+            root.addValue("value", i);
+        }
+        return root;
+    }
+
+    virtual void fromContainer( const Gpds::Container& object ) override
+    {
+        std::vector<int> tmp;
+        for (auto& value: object.getValues<int>("value")) {
+            tmp.push_back(value);
+        }
+        data = tmp;
+    }
+};
+
+TEST_CASE( "List order is consistent" )
+{
+    ListOrder list1;
+    for (int i = 0; i < 10; i++) {
+        list1.data.push_back(i);
+    }
+
+    // Serialize
+    std::stringstream serialized;
+    REQUIRE( GpdsTest::Test::serialize( serialized, list1, "listorder" ) );
+
+    // Deserialize
+    ListOrder list2;
+    REQUIRE( GpdsTest::Test::deserialize( serialized, list2, "listorder" ) );
+
+    // Ensure that data is the same
+    REQUIRE( list1.data == list2.data );
+}
+

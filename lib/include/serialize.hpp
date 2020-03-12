87c31596fe21735e28bc67ce747a34a4a6f7b9ab
Joel Bodenmann <joel.bodenmann@simulton.com>
2020-03-12 10:49:02 +0100

    Add serialize::to_file() and serialize::from_file()

--- lib/include/serialize.hpp
+++ lib/include/serialize.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <filesystem>
 #include "gpds_export.h"
 #include "container.hpp"
 
@@ -11,8 +12,13 @@ namespace gpds
     public:
         virtual ~serialize() = default;
 
+        // Container
         [[nodiscard]] virtual gpds::container to_container() const = 0;
         virtual void from_container(const gpds::container& container) = 0;
+
+        // File
+        std::pair<bool, std::string> to_file(const std::filesystem::path& path, const std::string& root_name) const;
+        std::pair<bool, std::string> from_file(const std::filesystem::path& path, const std::string& root_name);
     };
 
 }

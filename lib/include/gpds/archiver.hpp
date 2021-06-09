11f9ea07ec3c09e57a307ad680b2d9f8322044cd
Joel Bodenmann <joel.bodenmann@simulton.com>
2021-06-09 17:14:10 +0200

    Implement (de)serialization to/from std::string Fixes #6

--- lib/include/gpds/archiver.hpp
+++ lib/include/gpds/archiver.hpp
@@ -8,6 +8,12 @@
 namespace gpds
 {
 
+    /**
+     * Archiver base class.
+     *
+     * An archiver is responsible for (de)serialization.
+     * This class can be derived to implement (de)serialization for various formats such as XML and JSON.
+     */
     class GPDS_EXPORT archiver
     {
     public:
@@ -41,6 +47,17 @@ namespace gpds
         bool save(const std::filesystem::path& path, const serialize& object, const std::string& root_name) const
         {
             return save(path, object.to_container(), root_name);
+        }
+
+        bool save(std::string& str, const container& c, const std::string& root_name) const
+        {
+            std::ostringstream ss{ str };
+            return save(ss, c, root_name);
+        }
+
+        bool save(std::string& str, const serialize& obj, const std::string& root_name) const
+        {
+            return save(str, obj.to_container(), root_name);
         }
 
         virtual bool load(std::istream& stream, container& container, const std::string& rootName) = 0;
@@ -78,6 +95,24 @@ namespace gpds
                 return false;
 
             object.from_container(c);
+
+            return true;
+        }
+
+        bool load(const std::string& str, container& c, const std::string& root_name)
+        {
+            std::istringstream ss{ str };
+            return load(ss, c, root_name);
+        }
+
+        bool load(const std::string& str, serialize& obj, const std::string& root_name)
+        {
+            gpds::container c;
+            const bool successful = load(str, c, root_name);
+            if (!successful)
+                return false;
+
+            obj.from_container(c);
 
             return true;
         }

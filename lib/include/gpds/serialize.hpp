ff80d49c10a7591aca580e585304d390ea3f7c34
Joel Bodenmann <joel@insane.engineer>
2022-03-27 19:40:37 +0200

    serialize: add to_string() and from_string()

--- lib/include/gpds/serialize.hpp
+++ lib/include/gpds/serialize.hpp
@@ -19,6 +19,10 @@ namespace gpds
         [[nodiscard]] virtual gpds::container to_container() const = 0;
         virtual void from_container(const gpds::container& container) = 0;
 
+        // String
+        std::pair<bool, std::string> to_string(std::string& str, const std::string& root_name);
+        std::pair<bool, std::string> from_string(const std::string_view& str, const std::string& root_name);
+
         // File
         std::pair<bool, std::string> to_file(const std::filesystem::path& path, const std::string& root_name) const;
         std::pair<bool, std::string> from_file(const std::filesystem::path& path, const std::string& root_name);
--- lib/src/serialize.cpp
+++ lib/src/serialize.cpp
@@ -3,7 +3,26 @@
 
 using namespace gpds;
 
-std::pair<bool, std::string> serialize::to_file(const std::filesystem::path& path, const std::string& root_name) const
+std::pair<bool, std::string>
+serialize::to_string(std::string& str, const std::string& root_name)
+{
+    archiver_xml ar;
+    const bool& ret = ar.save(str, *this, root_name);
+
+    return { ret, "" };
+}
+
+std::pair<bool, std::string>
+serialize::from_string(const std::string_view& str, const std::string& root_name)
+{
+    archiver_xml ar;
+    const bool& ret = ar.load(std::string{str}, *this, root_name);        // ToDo: Add std::string_view interface to archiver_xml
+
+    return { ret, "" };
+}
+
+std::pair<bool, std::string>
+serialize::to_file(const std::filesystem::path& path, const std::string& root_name) const
 {
     archiver_xml ar;
     const bool& ret = ar.save(path, *this, root_name);
@@ -11,7 +30,8 @@ std::pair<bool, std::string> serialize::to_file(const std::filesystem::path& pat
     return { ret, "" };
 }
 
-std::pair<bool, std::string> serialize::from_file(const std::filesystem::path& path, const std::string& root_name)
+std::pair<bool, std::string>
+serialize::from_file(const std::filesystem::path& path, const std::string& root_name)
 {
     if (!std::filesystem::exists(path))
         return { false, "File does not exist." };

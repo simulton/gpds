785a1aea6b4a423aa64817758efca3f08fad7387
Joel Bodenmann <jbo@insane.engineer>
2023-07-14 16:44:36 +0200

    improve serialize interface
    Introduce new free-standing functions for (de)serialization to/from string and to/from file. Have the existing serialize::* functions wrap those and mark them as deprecated.

--- lib/include/gpds/serialize.hpp
+++ lib/include/gpds/serialize.hpp
@@ -3,6 +3,7 @@
 #include "container.hpp"
 
 #include <filesystem>
+#include <string>
 
 namespace gpds
 {
@@ -16,7 +17,7 @@ namespace gpds
         /**
          * The mode of the serialize.
          */
-        // ToDo: This is a bit whacky. We should ensure that all serializing interfaces accept the archiver as a template parameter instead.
+        // ToDo: Remove this once we removed all currently deprecated marked functions using this type.
         enum class mode
         {
             XML,
@@ -30,13 +31,104 @@ namespace gpds
         [[nodiscard]] virtual gpds::container to_container() const = 0;
         virtual void from_container(const gpds::container& container) = 0;
 
-        // String
-        std::pair<bool, std::string> to_string(std::string& str, std::string_view root_name, enum mode mode = mode::XML) const;
-        std::pair<bool, std::string> from_string(std::string_view str, std::string_view root_name, enum mode mode = mode::XML);
+        [[deprecated("use static gpds::to_string() instead.")]]
+        std::pair<bool, std::string>
+        to_string(std::string& str, std::string_view root_name, enum mode mode = mode::XML) const;
 
-        // File
-        std::pair<bool, std::string> to_file(const std::filesystem::path& path, std::string_view root_name, enum mode mode = mode::XML) const;
-        std::pair<bool, std::string> from_file(const std::filesystem::path& path, std::string_view root_name, enum mode mode = mode::XML);
+        [[deprecated("use static gpds::from_string() instead.")]]
+        std::pair<bool, std::string>
+        from_string(std::string_view str, std::string_view root_name, enum mode mode = mode::XML);
+
+        [[deprecated("use static gpds::to_file() instead.")]]
+        std::pair<bool, std::string>
+        to_file(const std::filesystem::path& path, std::string_view root_name, enum mode mode = mode::XML) const;
+
+        [[deprecated("use static gpds::from_file() instead.")]]
+        std::pair<bool, std::string>
+        from_file(const std::filesystem::path& path, std::string_view root_name, enum mode mode = mode::XML);
     };
 
+    template<typename Archiver, typename Object>
+    static
+    std::pair<bool, std::string>
+    to_string(std::string& str, const Object& obj, const std::string_view root_name)
+    {
+         bool ret = false;
+
+         Archiver ar;
+         ret = ar.save(str, obj, root_name);
+
+         return { ret, "" };
+    }
+
+    template<typename Archiver, typename Object>
+    static
+    std::pair<bool, std::string>
+    to_string(std::string& str, const Object& obj)
+    {
+        return to_string<Archiver, Object>(str, obj, Object::root_name);
+    }
+
+    template<typename Archiver, typename Object>
+    static
+    std::pair<bool, std::string>
+    from_string(std::string_view str, Object& obj, const std::string_view root_name)
+    {
+        bool ret = false;
+
+        Archiver ar;
+        ret = ar.load(std::string{str}, obj, root_name);     // ToDo: Add interface to archiver accepting std::string_view
+
+        return { ret, "" };
+    }
+
+    template<typename Archiver, typename Object>
+    static
+    std::pair<bool, std::string>
+    from_string(std::string_view str, Object& obj)
+    {
+        return from_string<Archiver, Object>(str, obj, Object::root_name);
+    }
+
+    template<typename Archiver, typename Object>
+    static
+    std::pair<bool, std::string>
+    to_file(const std::filesystem::path& path, const Object& obj, const std::string_view root_name)
+    {
+        bool ret = false;
+
+        Archiver ar;
+        ret = ar.save(path, obj, root_name);
+
+        return { ret, "" };
+    }
+
+    template<typename Archiver, typename Object>
+    static
+    std::pair<bool, std::string>
+    to_string(const std::filesystem::path& path, const Object& obj)
+    {
+        return to_file<Archiver, Object>(path, obj, Object::root_name);
+    }
+
+    template<typename Archiver, typename Object>
+    static
+    std::pair<bool, std::string>
+    from_file(const std::filesystem::path& path, Object& obj, const std::string_view root_name)
+    {
+        bool ret = false;
+
+        Archiver ar;
+        ret = ar.load(path, obj, root_name);
+
+        return { ret, "" };
+    }
+
+    template<typename Archiver, typename Object>
+    static
+    std::pair<bool, std::string>
+    from_string(const std::filesystem::path& path, Object& obj)
+    {
+        return from_file<Archiver, Object>(path, obj, Object::root_name);
+    }
 }
--- lib/src/serialize.cpp
+++ lib/src/serialize.cpp
@@ -7,75 +7,34 @@ using namespace gpds;
 std::pair<bool, std::string>
 serialize::to_string(std::string& str, const std::string_view root_name, enum mode mode) const
 {
-    bool ret = false;
     switch (mode)
     {
-    case mode::XML:
-        {
-            archiver_xml ar;
-            ret = ar.save(str, *this, root_name);
-        }
-        break;
-    case mode::YAML:
-        {
-            archiver_yaml ar;
-            ret = ar.save(str, *this, root_name);
-        }
-        break;
-    default:
-        return { ret, "Unsupported serialize format." };
+        case mode::XML:     return gpds::to_string<gpds::archiver_xml>(str, *this, root_name);
+        case mode::YAML:    return gpds::to_string<gpds::archiver_yaml>(str, *this, root_name);
+        default:            return { false, "unsupported serialization format." };
     }
-    return { ret, "" };
 }
 
 std::pair<bool, std::string>
 serialize::from_string(std::string_view str, const std::string_view root_name, enum mode mode)
 {
-    bool ret = false;
     switch (mode)
     {
-    case mode::XML:
-        {
-            archiver_xml ar;
-            // ToDo: Add std::string_view interface to archiver_xml
-            ret = ar.load(std::string{str}, *this, root_name);
-        }
-        break;
-    case mode::YAML:
-        {
-            archiver_yaml ar;
-            // ToDo: Add std::string_view interface to archiver_yaml
-            ret = ar.load(std::string{str}, *this, root_name);
-        }
-        break;
-    default:
-        return { ret, "Unsupported serialize format." };
+        case mode::XML:     return gpds::from_string<gpds::archiver_xml>(str, *this, root_name);
+        case mode::YAML:    return gpds::from_string<gpds::archiver_yaml>(str, *this, root_name);
+        default:            return { false, "unsupported serialization format." };
     }
-    return { ret, "" };
 }
 
 std::pair<bool, std::string>
 serialize::to_file(const std::filesystem::path& path, const std::string_view root_name, enum mode mode) const
 {
-    bool ret = false;
     switch (mode)
     {
-    case mode::XML:
-        {
-            archiver_xml ar;
-            ret = ar.save(path, *this, root_name);
-        }
-        break;
-    case mode::YAML:
-        {
-            archiver_yaml ar;
-            ret = ar.save(path, *this, root_name);
-        }
-        break;
-    default:
-        return { ret, "Unsupported serialize format." };
+        case mode::XML:     return gpds::to_file<gpds::archiver_xml>(path, *this, root_name);
+        case mode::YAML:    return gpds::to_file<gpds::archiver_yaml>(path, *this, root_name);
+        default:            return { false, "unsupported serialization format." };
     }
-    return { ret, "" };
 }
 
 std::pair<bool, std::string>
@@ -84,25 +43,10 @@ serialize::from_file(const std::filesystem::path& path, const std::string_view r
     if (!std::filesystem::exists(path))
         return { false, "File does not exist." };
 
-    bool ret = false;
-
     switch (mode)
     {
-    case mode::XML:
-        {
-            archiver_xml ar;
-            ret = ar.load(path, *this, root_name);
-        }
-        break;
-    case mode::YAML:
-        {
-            archiver_yaml ar;
-            ret = ar.load(path, *this, root_name);
-        }
-        break;
-    default:
-        return { ret, "Unsupported serialize format." };
+        case mode::XML:     return gpds::from_file<gpds::archiver_xml>(path, *this, root_name);
+        case mode::YAML:    return gpds::from_file<gpds::archiver_yaml>(path, *this, root_name);
+        default:            return { false, "unsupported serialization format." };
     }
-
-    return { ret, "" };
 }

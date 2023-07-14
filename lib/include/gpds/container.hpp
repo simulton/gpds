8412a224a268cd90c5c698ca349dcbd2a5b7b2bb
Joel Bodenmann <jbo@insane.engineer>
2023-07-14 16:05:40 +0200

    simplify
    This is a step towards a header-only library.

--- lib/include/gpds/container.hpp
+++ lib/include/gpds/container.hpp
@@ -198,7 +198,31 @@ namespace gpds
 
         [[nodiscard]]
         bool
-        is_list() const;
+        is_list() const
+        {
+            // We need at least two elements
+            if (values.size() < 2)
+                return false;
+
+            // Ensure that all elements are the same
+            std::string name;
+            for (auto it = values.cbegin(); it != values.cend(); ++it) {
+                // Store the name so we can compare them
+                if (it == values.begin())
+                    name = it->first;
+
+                // Must not be a container
+                if (it->second.is_type<container*>())
+                    return false;
+
+                // All elements need to share the same name
+                if (it->first != name)
+                    return false;
+            }
+
+            return true;
+        }
+
     };
 
 }

fc9360ace9aa6cb45287e7bb903c5007f7442e0d
Joel Bodenmann <jbo@insane.engineer>
2024-05-01 21:44:01 +0200

    concepts: Introduce Deserializable concept

--- lib/include/gpds/concepts.hpp
+++ lib/include/gpds/concepts.hpp
@@ -12,6 +12,13 @@ namespace gpds
         { T::gpds_name } -> std::convertible_to<std::string_view>;
 
         { t.to_container() } -> std::same_as<gpds::container>;
+    };
+
+    template<typename T>
+    concept Deserializable = requires(T t, gpds::container c)
+    {
+        { T::gpds_name } -> std::convertible_to<std::string_view>;
+
         { t.from_container(c) } -> std::same_as<void>;
     };
 
